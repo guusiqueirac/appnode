77670df10caebef6af20048a804be26f36f226f7
GUSTAVO SIQUEIRA COELHO <gustavo.scoelho@senacsp.edu.br>
2019-07-26 20:16:38 -0300

    first commit

---
+++ db.js
@@ -0,0 +1,27 @@
+var mongoClient = require("mongodb").MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+mongoClient.connect("mongodb://localhost:27017")
+    .then(conn => global.conn = conn.db("senac"))
+    .catch(err => console.log(err))
+
+function findAll(callback){
+    global.conn.collection("alunos").find({}).toArray(callback);
+}
+
+function findAlunos(id, callback){
+    global.conn.collection("alunos").findOne({_id: ObjectId(id)}, callback);
+}
+
+function deleteAlunos(id, callback){
+    global.conn.collection("alunos").remove({_id: ObjectId(id) }, callback);
+}
+
+function insertAlunos(alunos, callback){
+    global.conn.collection("alunos").insert(alunos, callback);
+}
+
+function updateAlunos(id, alunos, callback){
+    global.conn.collection("alunos").update({_id: ObjectId(id)}, alunos, callback);
+}
+module.exports = { findAll, findAlunos, deleteAlunos, insertAlunos, updateAlunos }
---
+++ routes/alunosnew.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.render('alunosnew', { title: 'Novo Aluno', alunos: {} });
+})
+
+router.get('/:id', function(req, res, next){
+    var db = require("../db");
+    db.findAlunos(req.params.id, function(err,doc){
+        if(err) return console.log(err)
+        res.render('alunosnew', { title: 'Editar Aluno', alunos:doc  });
+    })
+})
+
+router.post('/', function(req, res, next){
+    var id = req.body.id;
+    var nome = req.body.nome;
+    var idade = parseInt(req.body.idade);
+    var db = require("../db");
+    if(id === ''){
+
+        db.insertAlunos({nome, idade }, function(err,result){
+            if(err) return console.log(err);
+            res.redirect('/alunos/all');
+        })
+    } else {
+        db.updateAlunos(id, {nome,idade}, function(err,result){
+            if(err) return console.log(err);
+            res.redirect('/alunos/all');
+        })
+    }
+})
+
+module.exports = router;
